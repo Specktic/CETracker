e29b656c1bccd2dd27490cdf2eee719fd40d6487
Specktic <111095859+Specktic@users.noreply.github.com>
2022-11-21 21:53:42 -0600

    - Add random graph generator method to Graph.java

--- src/main/java/com/spring/cetracker/Graph.java
+++ src/main/java/com/spring/cetracker/Graph.java
@@ -1,39 +1,62 @@
 package com.spring.cetracker;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class Graph {
-    List<List<Node>> graphList = new ArrayList();
+    private Vertex[] vertices;
 
-    public Graph(List<Edge> edges) {
+    public Graph(int a) {
+        vertices = new Vertex[a];
 
+        for (int i = 0; i < a; i++) {
+            vertices[i] =new Vertex(i);
+        }
     }
 
-    static class Node {
+    public void addEdge(int start, int end, int weight) {
+        Edge edge = new Edge(start, end, weight);
+        vertices[start].addEdge(edge);
+    }
+
+    public Graph randomGraph(){
+        Random random = new Random();
+        int graphSize = random.nextInt(5, 20);
+
+        Graph map = new Graph(graphSize);
+
+        for (int i = 0; i < graphSize; i++) {
+            int start = random.nextInt(graphSize);
+            int end = random.nextInt(graphSize);
+            int weight = random.nextInt(1, 10);
+
+            map.addEdge(start, end, weight);
+        }
+
+        return map;
+    }
+
+    static class Vertex {
+        int id;
         Center center;
         LinkList adjacency = new LinkList();
 
-        public Node(Center center) {
-            this.center = center;
+        public Vertex(int id) {
+            this.id = id;
         }
 
         public void addEdge(Edge edge) {
             adjacency.add(edge);
         }
 
-        public LinkList getAdjacency() {return adjacency;}
-
         public Center getCenter() {return center;}
-
-        public void setCenter(Center center) {this.center = center;}
-
+        public LinkList getAdjacency() {return adjacency;}
+        public int getId() {return id;}
     }
 
     static class Edge {
-        private int start;
-        private int end;
-        private int weight;
+        int start;
+        int end;
+        int weight;
 
         public Edge(int start, int end, int weight) {
             this.start = start;
