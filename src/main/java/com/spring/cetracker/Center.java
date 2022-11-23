cdc61331941b143279adca33efb3614b77bb481a
Specktic <111095859+Specktic@users.noreply.github.com>
2022-11-22 20:59:07 -0600

    - Add Center.java array to Graph.java to match to vertex id

--- src/main/java/com/spring/cetracker/Center.java
+++ src/main/java/com/spring/cetracker/Center.java
@@ -2,4 +2,13 @@ package com.spring.cetracker;
 
 public class Center {
 
+    private String name;
+
+    public Center(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/com/spring/cetracker/Graph.java
+++ src/main/java/com/spring/cetracker/Graph.java
@@ -1,15 +1,30 @@
 package com.spring.cetracker;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Graph {
     private Vertex[] vertices;
+
+    private Center[] centers;
+
+    public Graph () {}
 
     public Graph(int a) {
         vertices = new Vertex[a];
 
         for (int i = 0; i < a; i++) {
             vertices[i] =new Vertex(i);
+        }
+    }
+
+    public Vertex[] getVertices() {return vertices;}
+
+    public Center[] getCenters() {return centers;}
+    public void setCenters(Center[] centers) {this.centers = centers;}
+    public void showCenters(){
+        for (Center center : centers) {
+            System.out.println(center.getName());
         }
     }
 
@@ -20,17 +35,26 @@ public class Graph {
 
     public Graph randomGraph(){
         Random random = new Random();
-        int graphSize = random.nextInt(5, 20);
+        int graphSize = random.nextInt(10, 20);
 
         Graph map = new Graph(graphSize);
+        Center[] cities = new Center[graphSize];
 
         for (int i = 0; i < graphSize; i++) {
+            cities[i] = new Center("Ciudad " + i);
+        }
+
+        for (int i = 0; i < (graphSize * 3); i++) {
             int start = random.nextInt(graphSize);
             int end = random.nextInt(graphSize);
             int weight = random.nextInt(1, 10);
 
             map.addEdge(start, end, weight);
         }
+
+        setCenters(cities);
+
+        showCenters();
 
         return map;
     }
